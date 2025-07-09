bc9e3c2420f5a0dee39b9627befc0222e2bffb38
MURACHIA JIMMY <jamesmurachia84@gmail.com>
2025-07-09 22:52:23 +0300

    BackendBaseURl and APIBaseUrl configured

--- client/src/services/backenInt.js
+++ client/src/services/backenInt.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { io } from "socket.io-client";
 
 
-const BackendBaseUrl = "http://localhost:5000";
-const APIBaseUrl = "http://localhost:5000/api";
+const BackendBaseUrl = "https://week-5-web-sockets-assignment-4o25.onrender.com";
+const APIBaseUrl = "https://week-5-web-sockets-assignment-4o25.onrender.com/api";
 
 const API = axios.create({
     baseURL: APIBaseUrl,
